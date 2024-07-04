c8f442e72d62d81fec782d31e74096f0085ff2e1
Roberquilez <159033774+Roberquilez@users.noreply.github.com>
2024-07-04 16:56:02 +0200

    cambios de enunciado

--- src/models/data.js
+++ src/models/data.js
@@ -5,26 +5,26 @@ import data from './data.json' assert { type: 'json' }
 // NOTA: Estas funciones únicamente deben exportar los datos del JSON
 //  sin procesarlos, como si de una BBDD se tratase
 
-export function getAlumnos() {
-    return data.alumnos
+export function getVehiculos() {
+    return data.vehiculos
 }
 
-export function addAlumno(alumno) {
+export function addVehiculo(vehiculo) {
     // TODO
 }
 
-export function getProfesores() {
-    return data.profesores
+export function getClientes() {
+    return data.clientes
 }
 
-export function addProfesor(profesor) {
+export function addAlquileres(alquileres) {
     // TODO
 }
 
-export function getAsignaturas() {
-    return data.asignaturas
+export function getAlquileres() {
+    return data.alquileres
 }
 
-export function addAsignatura(asignatura) {
+export function addVehiculos(vehiculos) {
     // TODO
 }
